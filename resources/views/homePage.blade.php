899717adf7ba90d982fba1f7b5e2edf4d6330c42
Zino Schubert <77681827+1010115@users.noreply.github.com>
2022-11-09 12:31:01 +0100

    Homepage gemaakt
    homepage buttons en styling toegevoegd

---
+++ resources/views/homePage.blade.php
@@ -0,0 +1,26 @@
+ <!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Page Title</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="/app.css">
+<body>
+
+<h1>App naam</h1>
+
+<div class>
+<h2>Welkom {$name} </h2>
+
+<a href="/melden.php"><button type="button"> MELDEN </button></a>
+<a href="/inbox.php"><button type="button"> INBOX </button></a>
+</div>
+
+
+<div class="feed">
+    <article>
+
+</div>
+
+</body>
+</html> 
+
