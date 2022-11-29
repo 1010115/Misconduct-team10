a2e9d769fa6023039c32b5dbf92aafb67e88a5a1
Zino Schubert <77681827+1010115@users.noreply.github.com>
2022-11-29 12:01:36 +0100

    Styling verbeterd

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -6,18 +6,34 @@
 <link rel="stylesheet" href="/app.css">
 <body>
 
-<h1>App naam</h1>
+<div class="body">
 
-<div class>
-<h2>Welkom {$name} </h2>
+<h2>Hoi {$name} </h2>
 
+<div class= "link-buttons">
 <a href="/melden.php"><button type="button"> MELDEN </button></a>
 <a href="/inbox.php"><button type="button"> INBOX </button></a>
 </div>
 
+<h2> Hoe gaat het vandaag?</h2>
+<div class="response-buttons">
+<button> Goed!</button>
+<button> Niet zo</button>
+<button> Weet ik niet </button>
+</div>
 
-<div class="feed">
+<div class"articles">
     <article>
+    <img >
+    <h1>Lorem ipsum </h1>
+    </article>
+
+    <article>
+    <img >
+    <h1>Lorem ipsum </h1>
+    </article>
+
+</div>
 
 </div>
 
