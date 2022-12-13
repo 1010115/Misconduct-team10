786278f6d6da8a32233e3055321cba6bd4ca1496
Zino Schubert <77681827+1010115@users.noreply.github.com>
2022-12-13 11:14:52 +0100

    Profile page aangemaakt
    Profile html page aangemaakt

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <meta charset="UTF-8">
-<title>Page Title</title>
+<title>Home</title>
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <link rel="stylesheet" href="/app.css">
 <body>
 
 <div class="body">
+<<<<<<< Updated upstream
     @foreach($users as $user)
         <h2>Hoi {{$user->name}} </h2>
     @endforeach
@@ -38,6 +39,33 @@
         </article>
 
     </div>
+=======
+
+<h2>Hoi {$name} </h2>
+
+<div class= "link-buttons">
+<a href="/melden.php"><button type="button"> MELDEN </button></a>
+<a href="/inbox.php"><button type="button"> INBOX </button></a>
+</div>
+
+<h2> Hoe gaat het vandaag?</h2>
+<div class="response-buttons">
+<button> Goed!</button>
+<button> Niet zo</button>
+<button> Weet ik niet </button>
+</div>
+
+<div class"articles">
+    <a href="/article.php">
+    <article>
+    <img >
+    <h1>Lorem ipsum </h1>
+    </article>
+    </a>
+
+
+</div>
+>>>>>>> Stashed changes
 
 </div>
 
